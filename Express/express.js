463b75e3ce73a3cd52de9579847c06001f1547eb
Nabaraj <nabaraj.dhl77@gmail.com>
2022-07-21 22:36:38 +0545

    REST API for /dishes added

--- Express/express.js
+++ Express/express.js
@@ -5,28 +5,9 @@ app.use(parser.json());
 const port = 3000;
 
 app.use(express.static(`${__dirname}/public`));
-app.all("/dishes", (req, res, next) => {
-  res.statusCode = 200;
-  (res.header = "Content-Type"), "plain-text";
-  next();
-});
-
-app.get("/dishes", (req, res) => {
-  res.end("You will get all the dishes list here");
-});
-
-app.post("/dishes", (req, res) => {
-  res.end(`You have orderes: ${req.body.name} and ${req.body.des}`);
-});
-
-app.put("/dishes", (req, res) => {
-  res.statusCode = 403;
-  res.end("Put not supported");
-});
-
-app.delete("/dishes", (req, res) => {
-  res.end("All Items Deleted");
-});
+//IMPORTING dishROuter
+const dishROuter = require("../routes/dishRouter");
+app.use("/dishes", dishROuter);
 
 app.listen(port, () => {
   console.log(` app listening on port ${port}`);
